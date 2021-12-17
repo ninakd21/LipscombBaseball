7b07a27c6d5752a0f9b46341fc076e33ded007a8
Nina Davis <nina.k.davis2@gmail>
2021-12-17 02:50:33 -0600

    updatedb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/codersOnly", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/LipscombStats", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const db = require("./config/connection");
 
 const { authMiddleware } = require("./utils/auth");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 const app = express();
 
 // create a new Apollo server and pass in our schema data
