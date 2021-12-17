9538f9a51214626333e5851d4f78997acd24ec7e
Nina Davis <nina.k.davis2@gmail>
2021-12-17 03:45:10 -0600

    fix

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import logoImage from '../../assets/logo.png';
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2021 by codersOnly</div>
+      <div className="container">
+        &copy;2021 Lipscomb University</div>
     </footer>
   );
 };
